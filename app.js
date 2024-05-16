529706516d5c91254722a9ee12910d78f3668d18
Rocmine <amini.rochd.hr@gmail.com>
2024-05-16 13:32:45 +0100

    update app js

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const ttvChat = document.querySelector('#chatting');
 // Twitch authentication parameters
 const client_id = "1lvh0n0oidy746dj9jl22t6xzbguo3";
 const redirect_uri = "https://rocmine.github.io/BasicTwitchDashboard/dash";
-const scope = ["user:read:follows","channel:read:subscriptions"]; // Adjust scopes as needed
-scope.join(" ");
+const scope = ["user:read:follows channel:read:subscriptions"]; // Adjust scopes as needed
 let user_id=null;
 
 // Function to redirect user to Twitch authentication page
