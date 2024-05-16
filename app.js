f34b614bcddf700fa6e67ed3cb11d0de20fcd71e
Rocmine <amini.rochd.hr@gmail.com>
2024-05-16 11:21:12 +0100

    update app js

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ function handleTwitchCallback() {
     if (sessionStorage.getItem("token") == accessToken) {
         // Use the access token to make requests to the Twitch API
         ttvChat.setAttribute("src",`https://www.twitch.tv/popout/${user_id}/chat`);
-        ttvChat.console.log = function() { /* nop */ };
         fetchingInfo(client_id,sessionStorage.getItem("token"));
         changeURL(`BasicTwitchDashboard - ${user_id}` ,"dash");
         setInterval(() => {fetchingInfo(client_id,sessionStorage.getItem("token"));},30000);
